9a6842fd0f2094aba019aab0a9dca618c84a047a
Adithya Aravind <adithya@Adithyas-MacBook-Pro.local>
2024-10-23 03:19:05 -0400

    tst: blur fix

--- assignments/assignment4/test/ImageUtilityTest.java
+++ assignments/assignment4/test/ImageUtilityTest.java
@@ -5,7 +5,7 @@ import java.io.File;
 
 import javax.imageio.ImageIO;
 
-import factories.ImageFactory;
+
 import factories.ImageReaderFactory;
 import factories.ImageWriterFactory;
 import filters.Blur;
@@ -17,69 +17,68 @@ import model.visual.Image;
 
 import static org.junit.Assert.*;
 
-import utility.ImageUtility;
 
 
 public class ImageUtilityTest {
 
-    @Test
-    public void testReadLocalImage() {
-      try {
-        File file = new File("test_resources/download.jpeg");
-        BufferedImage expectedImage = ImageIO.read(file);
+  @Test
+  public void testReadLocalImage() {
+    try {
+      File file = new File("test_resources/download.jpeg");
+      BufferedImage expectedImage = ImageIO.read(file);
 
-        Image actualImage = ImageReaderFactory.createImageReader(ImageType.JPEG).read(file.getPath());
+      Image actualImage = ImageReaderFactory.createImageReader(ImageType.JPEG).read(file.getPath());
 
-        int width = expectedImage.getWidth();
-        int height = expectedImage.getHeight();
+      int width = expectedImage.getWidth();
+      int height = expectedImage.getHeight();
 
-        for (int y = 0; y < height; y++) {
-          for (int x = 0; x < width; x++) {
-            int expectedPixel = expectedImage.getRGB(x, y);
-            int expectedRed = (expectedPixel >> 16) & 0xff;
-            int expectedGreen = (expectedPixel >> 8) & 0xff;
-            int expectedBlue = expectedPixel & 0xff;
+      for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+          int expectedPixel = expectedImage.getRGB(x, y);
+          int expectedRed = (expectedPixel >> 16) & 0xff;
+          int expectedGreen = (expectedPixel >> 8) & 0xff;
+          int expectedBlue = expectedPixel & 0xff;
 
-            Pixel actualPixel = actualImage.getPixel(x, y);
-            assertEquals(expectedRed, actualPixel.getRed());
-            assertEquals(expectedGreen, actualPixel.getGreen());
-            assertEquals(expectedBlue, actualPixel.getBlue());
-          }
+          Pixel actualPixel = actualImage.getPixel(x, y);
+          assertEquals(expectedRed, actualPixel.getRed());
+          assertEquals(expectedGreen, actualPixel.getGreen());
+          assertEquals(expectedBlue, actualPixel.getBlue());
         }
-      } catch (Exception e) {
-        fail("Exception should not be thrown: " + e.getMessage());
       }
+    } catch (Exception e) {
+      fail("Exception should not be thrown: " + e.getMessage());
+    }
   }
 
   @Test
-    public void testSaveImage() {
-        try {
-            // Load an image from the test resources
-            File inputFile = new File("test_resources/download.jpeg");
-            Image image = ImageReaderFactory.createImageReader(ImageType.JPEG).read(inputFile.getPath());
+  public void testSaveImage() {
+    try {
+      // Load an image from the test resources
+      File inputFile = new File("test_resources/download.jpeg");
+      Image image = ImageReaderFactory.createImageReader(ImageType.JPEG).read(inputFile.getPath());
 
-            // Save the image to a new file
-            String outputPath = "test_resources/download-output.png";
-            ImageWriterFactory.createImageWriter(ImageType.PNG).write(image,outputPath);
+      // Save the image to a new file
+      String outputPath = "test_resources/download-output.png";
+      ImageWriterFactory.createImageWriter(ImageType.PNG).write(image, outputPath);
 
-            // Load the saved image
-            File outputFile = new File(outputPath);
-            BufferedImage savedImage = ImageIO.read(outputFile);
+      // Load the saved image
+      File outputFile = new File(outputPath);
+      BufferedImage savedImage = ImageIO.read(outputFile);
 
-            // Compare the original and saved images
-            BufferedImage originalImage = ImageIO.read(inputFile);
-            assertEquals(originalImage.getWidth(), savedImage.getWidth());
-            assertEquals(originalImage.getHeight(), savedImage.getHeight());
+      // Compare the original and saved images
+      BufferedImage originalImage = ImageIO.read(inputFile);
+      assertEquals(originalImage.getWidth(), savedImage.getWidth());
+      assertEquals(originalImage.getHeight(), savedImage.getHeight());
 
-            for (int y = 0; y < originalImage.getHeight(); y++) {
-                for (int x = 0; x < originalImage.getWidth(); x++) {
-                    assertEquals(originalImage.getRGB(x, y), savedImage.getRGB(x, y));
-                }
-            }
-        } catch (Exception e) {
-            fail("Exception should not be thrown: " + e.getMessage());
+      for (int y = 0; y < originalImage.getHeight(); y++) {
+        for (int x = 0; x < originalImage.getWidth(); x++) {
+          assertEquals(originalImage.getRGB(x, y), savedImage.getRGB(x, y));
         }
+      }
+    } catch (Exception e) {
+      fail("Exception should not be thrown: " + e.getMessage());
     }
+  }
 
   @Test
   public void testSaveSepiaImage() {
@@ -90,7 +89,7 @@ public class ImageUtilityTest {
 
       // Save the image to a new file
       String outputPath = "test_resources/manhattan-small-sepia-output.png";
-      ImageWriterFactory.createImageWriter(ImageType.PNG).write(image.getSepia(),outputPath);
+      ImageWriterFactory.createImageWriter(ImageType.PNG).write(image.getSepia(), outputPath);
 
       // Load the saved image
       File outputFile = new File(outputPath);
@@ -124,7 +123,7 @@ public class ImageUtilityTest {
 
       // Save the image to a new file
       String outputPath = "test_resources/manhattan-small-luma.png";
-      ImageWriterFactory.createImageWriter(ImageType.PNG).write(image.getLuma(),outputPath);
+      ImageWriterFactory.createImageWriter(ImageType.PNG).write(image.getLuma(), outputPath);
 
       // Load the saved image
       File outputFile = new File(outputPath);
@@ -158,7 +157,7 @@ public class ImageUtilityTest {
 
       // Save the image to a new file
       String outputPath = "test_resources/manhattan-small-intensity.png";
-      ImageWriterFactory.createImageWriter(ImageType.PNG).write(image.getIntensity(),outputPath);
+      ImageWriterFactory.createImageWriter(ImageType.PNG).write(image.getIntensity(), outputPath);
 
       // Load the saved image
       File outputFile = new File(outputPath);
@@ -191,7 +190,7 @@ public class ImageUtilityTest {
       Image image = ImageReaderFactory.createImageReader(ImageType.JPEG).read(inputFile.getPath());
       // Save the image to a new file
       String outputPath = "test_resources/manhattan-small-value.png";
-      ImageWriterFactory.createImageWriter(ImageType.PNG).write(image.getValue(),outputPath);
+      ImageWriterFactory.createImageWriter(ImageType.PNG).write(image.getValue(), outputPath);
 
       // Load the saved image
       File outputFile = new File(outputPath);
@@ -226,12 +225,11 @@ public class ImageUtilityTest {
       // Save the image to a new file
       String outputPath = "test_resources/manhattan-small-blur.png";
       Filter blurFilter = new Blur();
-      ImageWriterFactory.createImageWriter(ImageType.PNG).write(blurFilter.applyFilter(image),outputPath);
+      ImageWriterFactory.createImageWriter(ImageType.PNG).write(blurFilter.applyFilter(blurFilter.applyFilter(image)), outputPath);
 
       // Load the saved image
       File outputFile = new File(outputPath);
       BufferedImage savedImage = ImageIO.read(outputFile);
-
       // Load the expected result image
       String resultPath = "test_resources/expected_results/manhattan-small-blur-2.png";
       File expectedFile = new File(resultPath);
