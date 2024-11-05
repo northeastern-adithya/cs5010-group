93f91484293b662adf68925d6ae93a6f688b041e
Adithya Aravind <adithya@Adithyas-MacBook-Pro.local>
2024-11-05 17:30:47 -0500

    feat: scripting and interactive mode

--- assignments/assignment5/src/app/ImageProcessorApp.java
+++ assignments/assignment5/src/app/ImageProcessorApp.java
@@ -2,10 +2,14 @@ package app;
 
 
 import java.io.InputStreamReader;
+import java.io.StringReader;
 
 import controller.ImageProcessorController;
 import exception.ImageProcessingRunTimeException;
 import factories.Factory;
+import model.enumeration.UserCommand;
+import utility.StringUtils;
+import view.input.UserInput;
 
 /**
  * The main class for the Image Processor application.
@@ -21,7 +25,7 @@ public class ImageProcessorApp {
    */
   public static void main(String[] args) {
     ImageProcessorController controller = Factory.createController(
-            Factory.createUserInput(new InputStreamReader(System.in)),
+            Factory.createUserInput(createReadableFromArgs(args)),
             Factory.createUserOutput(System.out),
             Factory.createImageProcessor(Factory.getImageMemory())
     );
@@ -32,5 +36,35 @@ public class ImageProcessorApp {
         break;
       }
     }
+  }
+
+  /**
+   * Creates a Readable object from the command line arguments.
+   * If command line arguments are not provided, it reads from the standard
+   * input.
+   * File name is expected as the first argument.
+   *
+   * @param args The command line arguments.
+   * @return a readable object to read inputs from.
+   */
+  private static Readable createReadableFromArgs(String[] args) {
+    if (args.length == 0 || StringUtils.isNullOrEmpty(args[0])) {
+      return new InputStreamReader(System.in);
+    } else {
+      return formRunScriptCommand(args[0]);
+    }
+  }
+
+  /**
+   * Forms a command to run a script file.
+   * Has a default input as run <fileName> quit.
+   *
+   * @param fileName The name of the file to run with its location.
+   * @return a readable object to read inputs from.
+   */
+  private static Readable formRunScriptCommand(String fileName) {
+    return new StringReader(String.format("%s %s %s",
+            UserCommand.RUN.getCommand(),
+            fileName, UserCommand.QUIT.getCommand()));
   }
 }
