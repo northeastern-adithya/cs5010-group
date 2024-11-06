3b398cd3ce9b8005a124223cca1ab4b5925aa03b
Ishan Rai <51855687+ishanrai17@users.noreply.github.com>
2024-11-06 16:48:23 -0500

    rft: Javastyle Fix (#31)
    Co-authored-by: Ishan Rai <ishanrai@Ishans-MacBook-Pro.local>

--- assignments/assignment5/src/model/enumeration/ControllerType.java
+++ assignments/assignment5/src/model/enumeration/ControllerType.java
@@ -1,6 +1,15 @@
 package model.enumeration;
 
+/**
+ * Enumeration representing the types of controllers.
+ */
 public enum ControllerType {
+  /**
+   * Interactive controller type.
+   */
   INTERACTIVE,
+  /**
+   * Command line controller type.
+   */
   COMMAND_LINE;
 }
