8e36823a9059d4619e450de1ff6fe973e8ae2ee6
Ishan Rai <ishanrai@Ishans-MacBook-Pro.local>
2024-12-02 22:23:33 -0500

    Unit Tests

--- assignments/assignment7/test/model/PNGTest.java
+++ assignments/assignment7/test/model/PNGTest.java
@@ -13,6 +13,157 @@ import static org.junit.Assert.assertTrue;
 public class PNGTest {
 
   @Test
+  public void testDithering() {
+    ImageImplementation imgImpl = new ImageImplementation();
+    Pixel[][] randomPixels = new Pixel[][] {
+            { new Pixel(123, 234, 45), new Pixel(67, 89, 12), new Pixel(34, 56, 78) },
+            { new Pixel(90, 123, 234), new Pixel(56, 78, 90), new Pixel(12, 34, 56) }
+    };
+    imgImpl.setImage(randomPixels);
+    imgImpl.applyDithering();
+
+    Pixel[][] actual = imgImpl.getImage();
+
+    Pixel[][] expectedResult = new Pixel[][] {
+            { new Pixel(255, 255, 255), new Pixel(0, 0, 0), new Pixel(0, 0, 0) },
+            { new Pixel(0, 0, 0), new Pixel(0, 0, 0), new Pixel(0, 0, 0) }
+    };
+
+    for(int i = 0; i < randomPixels.length; i++) {
+      for(int j = 0; j < randomPixels[0].length; j++) {
+        assertEquals(expectedResult[i][j].get(0), actual[i][j].get(0));
+        assertEquals(expectedResult[i][j].get(1), actual[i][j].get(1));
+        assertEquals(expectedResult[i][j].get(2), actual[i][j].get(2));
+      }
+    }
+  }
+
+  @Test
+  public void testDitheringWithSplit100() {
+    ImageImplementation imgImpl = new ImageImplementation();
+    Pixel[][] randomPixels = new Pixel[][] {
+            { new Pixel(123, 234, 45), new Pixel(67, 89, 12), new Pixel(34, 56, 78) },
+            { new Pixel(90, 123, 234), new Pixel(56, 78, 90), new Pixel(12, 34, 56) }
+    };
+    imgImpl.setImage(randomPixels);
+    imgImpl.applyDithering(100);
+
+    Pixel[][] actual = imgImpl.getImage();
+
+    Pixel[][] expectedResult = new Pixel[][] {
+            { new Pixel(255, 255, 255), new Pixel(0, 0, 0), new Pixel(0, 0, 0) },
+            { new Pixel(0, 0, 0), new Pixel(0, 0, 0), new Pixel(0, 0, 0) }
+    };
+
+    for(int i = 0; i < randomPixels.length; i++) {
+      for(int j = 0; j < randomPixels[0].length; j++) {
+        assertEquals(expectedResult[i][j].get(0), actual[i][j].get(0));
+        assertEquals(expectedResult[i][j].get(1), actual[i][j].get(1));
+        assertEquals(expectedResult[i][j].get(2), actual[i][j].get(2));
+      }
+    }
+  }
+
+  @Test
+  public void testDitheringWithSplit50() {
+    ImageImplementation imgImpl = new ImageImplementation();
+    Pixel[][] randomPixels = new Pixel[][] {
+            { new Pixel(123, 234, 45), new Pixel(67, 89, 12), new Pixel(34, 56, 78) },
+            { new Pixel(90, 123, 234), new Pixel(56, 78, 90), new Pixel(12, 34, 56) }
+    };
+    imgImpl.setImage(randomPixels);
+    imgImpl.applyDithering(50);
+
+    Pixel[][] actual = imgImpl.getImage();
+
+    Pixel[][] expectedResult = new Pixel[][] {
+            { new Pixel(255, 255, 255), new Pixel(0, 0, 0), new Pixel(34, 56, 78) },
+            { new Pixel(0, 0, 0), new Pixel(0, 0, 0), new Pixel(12, 34, 56) }
+    };
+
+    for(int i = 0; i < randomPixels.length; i++) {
+      for(int j = 0; j < randomPixels[0].length; j++) {
+        assertEquals(expectedResult[i][j].get(0), actual[i][j].get(0));
+        assertEquals(expectedResult[i][j].get(1), actual[i][j].get(1));
+        assertEquals(expectedResult[i][j].get(2), actual[i][j].get(2));
+      }
+    }
+  }
+
+  @Test
+  public void testDitheringWithSplit25() {
+    ImageImplementation imgImpl = new ImageImplementation();
+    Pixel[][] randomPixels = new Pixel[][] {
+            { new Pixel(123, 234, 45), new Pixel(67, 89, 12), new Pixel(34, 56, 78) },
+            { new Pixel(90, 123, 234), new Pixel(56, 78, 90), new Pixel(12, 34, 56) }
+    };
+    imgImpl.setImage(randomPixels);
+    imgImpl.applyDithering(25);
+
+    Pixel[][] actual = imgImpl.getImage();
+
+    Pixel[][] expectedResult = new Pixel[][] {
+            { new Pixel(0, 0, 0), new Pixel(67, 89, 12), new Pixel(34, 56, 78) },
+            { new Pixel(0, 0, 0), new Pixel(56, 78, 90), new Pixel(12, 34, 56) }
+    };
+
+    for(int i = 0; i < randomPixels.length; i++) {
+      for(int j = 0; j < randomPixels[0].length; j++) {
+        assertEquals(expectedResult[i][j].get(0), actual[i][j].get(0));
+        assertEquals(expectedResult[i][j].get(1), actual[i][j].get(1));
+        assertEquals(expectedResult[i][j].get(2), actual[i][j].get(2));
+      }
+    }
+  }
+
+  @Test
+  public void testDitheringWithSplit75() {
+    ImageImplementation imgImpl = new ImageImplementation();
+    Pixel[][] randomPixels = new Pixel[][] {
+            { new Pixel(123, 234, 45), new Pixel(67, 89, 12), new Pixel(34, 56, 78) },
+            { new Pixel(90, 123, 234), new Pixel(56, 78, 90), new Pixel(12, 34, 56) }
+    };
+    imgImpl.setImage(randomPixels);
+    imgImpl.applyDithering(75);
+
+    Pixel[][] actual = imgImpl.getImage();
+
+    Pixel[][] expectedResult = new Pixel[][] {
+            { new Pixel(255, 255, 255), new Pixel(0, 0, 0), new Pixel(0, 0, 0) },
+            { new Pixel(0, 0, 0), new Pixel(0, 0, 0), new Pixel(0, 0, 0) }
+    };
+
+    for(int i = 0; i < randomPixels.length; i++) {
+      for(int j = 0; j < randomPixels[0].length; j++) {
+        assertEquals(expectedResult[i][j].get(0), actual[i][j].get(0));
+        assertEquals(expectedResult[i][j].get(1), actual[i][j].get(1));
+        assertEquals(expectedResult[i][j].get(2), actual[i][j].get(2));
+      }
+    }
+  }
+
+  @Test
+  public void testDitheringWithSplit0() {
+    ImageImplementation imgImpl = new ImageImplementation();
+    Pixel[][] randomPixels = new Pixel[][] {
+            { new Pixel(123, 234, 45), new Pixel(67, 89, 12), new Pixel(34, 56, 78) },
+            { new Pixel(90, 123, 234), new Pixel(56, 78, 90), new Pixel(12, 34, 56) }
+    };
+    imgImpl.setImage(randomPixels);
+    imgImpl.applyDithering(0);
+
+    Pixel[][] actual = imgImpl.getImage();
+
+    for(int i = 0; i < randomPixels.length; i++) {
+      for(int j = 0; j < randomPixels[0].length; j++) {
+        assertEquals(randomPixels[i][j].get(0), actual[i][j].get(0));
+        assertEquals(randomPixels[i][j].get(1), actual[i][j].get(1));
+        assertEquals(randomPixels[i][j].get(2), actual[i][j].get(2));
+      }
+    }
+  }
+
+  @Test
   public void testLoadPNGImageSuccess() {
     ImageImplementation imgImpl = new ImageImplementation();
     Pixel[][] image = ImageUtil.loadImage("res/PNG/flower.png");
